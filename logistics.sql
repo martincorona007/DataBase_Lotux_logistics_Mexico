69f7c6fc824ac8e96b409018ef4fe26eadde5e53
Martin Corona <31390537+martincorona007@users.noreply.github.com>
2019-05-13 19:07:35 -0500

    2.2

--- logistics.sql
+++ logistics.sql
@@ -306,15 +306,25 @@ CREATE TRIGGER before_Delete_To_WareHouse_Backup AFTER DELETE ON Warehouse
 FOR EACH ROW
 INSERT INTO History_Warehouse(ID_wh,FK_ID_client, FK_ID_dt, deliveryDate, deliveryBy, cost, description, shape, size, weight, physical_features, number_units,status) VALUES(OLD.ID_wh,OLD.FK_ID_client,OLD.FK_ID_dt,OLD.deliveryDate,OLD.deliveryBy,OLD.cost,OLD.description,OLD.shape,OLD.size,OLD.weight,OLD.physical_features,OLD.number_units,OLD.status);
 
-
 DELETE FROM Product WHERE ID_prod="3";
+DELETE FROM Product WHERE ID_prod="8";
+DELETE FROM Product WHERE ID_prod="6";
+DELETE FROM Product WHERE ID_prod="10";
+DELETE FROM Product WHERE ID_prod="12";
+DELETE FROM Product WHERE ID_prod="5";
 
 
 
-UPDATE Warehouse SET FK_ID_Int_transp="5",FK_ID_Ext_transp="2",status="warehause" WHERE ID_wh="1";
 
-UPDATE Warehouse SET status="delivered" WHERE ID_wh="1";
+UPDATE Warehouse SET FK_ID_Int_transp="4",FK_ID_Ext_transp="8",status="warehause" WHERE ID_wh="2";
+UPDATE Warehouse SET FK_ID_Int_transp="9",FK_ID_Ext_transp="6",status="warehause" WHERE ID_wh="4";
+UPDATE Warehouse SET FK_ID_Int_transp="15",FK_ID_Ext_transp="14",status="warehause" WHERE ID_wh="6";
 
 
-DELETE FROM Warehouse WHERE ID_wh="1";
+UPDATE Warehouse SET status="delivered" WHERE ID_wh="2";
+UPDATE Warehouse SET status="delivered" WHERE ID_wh="6";
+
+
+DELETE FROM Warehouse WHERE ID_wh="2";
+DELETE FROM Warehouse WHERE ID_wh="6";
 
